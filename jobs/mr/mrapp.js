7537f71c30d17ba4fcb1720bfc40e108c0e1202d
Kamran Ali Khan <khankamranali@gmail.com>
2014-06-04 18:52:25 +0530

    mr user password added

--- jobs/mr/mrapp.js
+++ jobs/mr/mrapp.js
@@ -2,8 +2,20 @@ const
 	mr = require('./mrjob');
 	cron = require('cron');
 	logger = require('morgan'),
-	mongo = require('mongoskin'),
-	db = mongo.db("mongodb://localhost:27017/perf", {auto_reconnect: true, native_parser: true});
+	mongo = require('mongoskin');
+
+//provide a sensible default for local development
+var mongodbConnectionString = "mongodb://localhost:27017/perf"
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodbConnectionString = process.env.OPENSHIFT_MONGODB_DB_URL + 'webperf';
+}
+var db = mongo.db(mongodbConnectionString, {auto_reconnect: true, native_parser: true});
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  db.authenticate('admin', '6WPBZjxMQyys', function(err, res) {
+	console.log('Mongodb connection auth passed.');
+  });
+}
 
 
 var cronJob = cron.job("0/10 * * * * *", function(){
