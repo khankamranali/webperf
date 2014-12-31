8be2ef7bcebf94f1b22a9851168f7e5cf88ea07f
Kamran Ali Khan <khankamranali@gmail.com>
2014-12-31 17:02:49 +0530

    removed .openshift files

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var options = {
 };
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 http.createServer(app).listen(port, ip, function () {
   console.log( "Listening on ip=" + ip + ", port=" + port )
